93197badf2367776a3102674506fd3ab4ca444a6
thaikietGh6699 <kiet692002@gmail.com>
2024-08-25 18:01:44 +0700

    hotel

---
+++ APP/Data/DatabaseHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APP.Models;
+using MySql.Data.MySqlClient;
+
+namespace APP.Data
+{
+    internal class DatabaseHelper
+    {
+        private string connectionString = "Server=localhost;Database=GymManagement;Uid=APP1;Pwd=12345;";
+
+        public IEnumerable<Member> GetMembers()
+        {
+            var members = new List<Member>();
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("SELECT * FROM Members", connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        members.Add(new Member
+                        {
+                            MemberID = reader.GetInt32("MemberID"),
+                            FullName = reader.GetString("FullName"),
+                            PhoneNumber = reader.GetString("PhoneNumber"),
+                            MembershipType = reader.GetString("MembershipType"),
+                            MembershipStartDate = reader.GetDateTime("MembershipStartDate"),
+                            MembershipEndDate = reader.GetDateTime("MembershipEndDate")
+                        });
+                    }
+                }
+            }
+            return members;
+        }
+
+        public void AddMember(Member member)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("INSERT INTO Members (FullName, PhoneNumber, MembershipType, MembershipStartDate, MembershipEndDate) VALUES (@FullName, @PhoneNumber, @MembershipType, @MembershipStartDate, @MembershipEndDate)", connection);
+                command.Parameters.AddWithValue("@FullName", member.FullName);
+                command.Parameters.AddWithValue("@PhoneNumber", member.PhoneNumber);
+                command.Parameters.AddWithValue("@MembershipType", member.MembershipType);
+                command.Parameters.AddWithValue("@MembershipStartDate", member.MembershipStartDate);
+                command.Parameters.AddWithValue("@MembershipEndDate", member.MembershipEndDate);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateMember(Member member)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("UPDATE Members SET FullName = @FullName, PhoneNumber = @PhoneNumber, MembershipType = @MembershipType, MembershipStartDate = @MembershipStartDate, MembershipEndDate = @MembershipEndDate WHERE MemberID = @MemberID", connection);
+                command.Parameters.AddWithValue("@FullName", member.FullName);
+                command.Parameters.AddWithValue("@PhoneNumber", member.PhoneNumber);
+                command.Parameters.AddWithValue("@MembershipType", member.MembershipType);
+                command.Parameters.AddWithValue("@MembershipStartDate", member.MembershipStartDate);
+                command.Parameters.AddWithValue("@MembershipEndDate", member.MembershipEndDate);
+                command.Parameters.AddWithValue("@MemberID", member.MemberID);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteMember(int memberId)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("DELETE FROM Members WHERE MemberID = @MemberID", connection);
+                command.Parameters.AddWithValue("@MemberID", memberId);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void ExtendMembership(int memberId)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("UPDATE Members SET MembershipEndDate = DATE_ADD(MembershipEndDate, INTERVAL 30 DAY) WHERE MemberID = @MemberID", connection);
+                command.Parameters.AddWithValue("@MemberID", memberId);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public decimal GetTotalRevenue()
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("SELECT SUM(Amount) FROM Revenue", connection);
+                var result = command.ExecuteScalar();
+                return result != DBNull.Value ? Convert.ToDecimal(result) : 0;
+            }
+        }
+
+    }
+}
---
+++ APP/ViewModels/MemberViewModel.cs
@@ -0,0 +1,97 @@
+﻿using APP.Commands;
+using APP.Data;
+using APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace APP.ViewModels
+{
+    internal class MemberViewModel 
+    {
+        private DatabaseHelper databaseHelper;
+        public ObservableCollection<Member> Members { get; set; }   
+        public ICommand LoadMembersCommand { get; set; }
+        public ICommand AddMembersCommand { get; set; }
+        public ICommand UpdateMembersCommand { get; set; }
+        public ICommand DeleteMembersCommand { get; set; }
+        public ICommand ExtendMembersCommand { get; set; }
+
+        private Member _selectedMember;
+        public Member SelectedMember
+        {
+            get => _selectedMember;
+            set
+            {
+                _selectedMember = value;
+                OnPropertyChanged();
+            }
+        }
+        public MemberViewModel()
+        {
+            databaseHelper = new DatabaseHelper();
+            Members = new ObservableCollection<Member>();
+            LoadMembersCommand = new RelayCommand(LoadMembers);
+            AddMembersCommand = new RelayCommand(AddMember);
+            UpdateMembersCommand = new RelayCommand(UpdateMember);
+            DeleteMembersCommand = new RelayCommand(DeleteMember);
+            ExtendMembersCommand = new RelayCommand(ExtendMembership);
+        }
+        private void LoadMembers()
+        {
+            var members = databaseHelper.GetMembers();
+            Members.Clear();
+            foreach (var member in members)
+            {
+                Members.Add(member);
+            }
+        }
+        private void AddMember()
+        {
+            if (SelectedMember != null)
+            {
+                databaseHelper.AddMember(SelectedMember);
+                LoadMembers();
+            }
+        }
+
+        private void UpdateMember()
+        {
+            if (SelectedMember != null)
+            {
+                databaseHelper.UpdateMember(SelectedMember);
+                LoadMembers();
+            }
+        }
+
+        private void DeleteMember()
+        {
+            if (SelectedMember != null)
+            {
+                databaseHelper.DeleteMember(SelectedMember.MemberID);
+                LoadMembers();
+            }
+        }
+
+        private void ExtendMembership()
+        {
+            if (SelectedMember != null)
+            {
+                databaseHelper.ExtendMembership(SelectedMember.MemberID);
+                LoadMembers();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
